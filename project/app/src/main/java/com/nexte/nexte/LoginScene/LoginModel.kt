fa96a06cfd74bafd50bcf36293b1ae02783e61a7
Baldissera <guilhermebaldissera@hotmail.com>
2018-03-24 19:42:13 -0300

    Started Login Scene * Created Interactor Class * Created Interactor Interface - LoginBusinessLogic * Created Model Class - LoginModel * Created View Class - LoginView
    Co-authored-by: Lorrany Freire <lorranyfreire@hotmaail.com>
    Co-authored-by: Gabriel Albino <enggabrielalbino@gmail.com>
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>
    Co-authored-by: Helena Goulart <helenabretasg@gmail.com>
    Co-authored-by: Alexandre Miguel <aleronupe@gmail.com>
    Co-authored-by: Letícia Meneses <lmbs.geo@gmail.com>
    Co-authored-by: Miguel Pimentel <miguelpimentel2013@gmail.com>
    Co-authored-by: Luis Gustavo <luis.gustavo3013@hotmail.com>
    Co-authored-by: Geovanni Oliveira <geovannirock@gmail.com>

---
+++ project/app/src/main/java/com/nexte/nexte/LoginScene/LoginModel.kt
@@ -0,0 +1,35 @@
+package com.nexte.nexte.LoginScene
+
+/**
+ * Created by baldissera on 24/03/2018.
+ */
+class LoginModel {
+
+    class Request(userName: String, password: String) {
+        var userName: String = ""
+        var password: String = ""
+
+        init {
+            this.userName = userName
+            this.password = password
+        }
+
+    }
+    class Response(tokenId: String){
+        var tokenId: String = ""
+
+        init {
+            this.tokenId = tokenId
+        }
+
+    }
+
+    class ViewModel(message: String){
+        var message: String = ""
+
+        init{
+            this.message = message
+        }
+
+    }
+}
