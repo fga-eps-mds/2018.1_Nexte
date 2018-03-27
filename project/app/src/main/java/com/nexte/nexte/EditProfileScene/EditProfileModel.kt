a6872e7fc604e33b389c4c0f3f426ebd6108a7de
Lorrany Freire <lorranyfreire@hotmail.com>
2018-03-27 15:51:53 -0300

    Created edit profile model > * Added Request, Response and ViewModel > > Co-authored-by: Gabriel Albino <enggabrielalbino@gmail.com>

---
+++ project/app/src/main/java/com/nexte/nexte/EditProfileScene/EditProfileModel.kt
@@ -0,0 +1,45 @@
+package com.nexte.nexte.EditProfileScene
+
+/**
+ * Created by albino on 27/03/18.
+ */
+class EditProfileModel {
+    class Response {
+        var username: String = ""
+        var rank: Int = -1
+        var profilePictureURL: String = ""
+        var club: String = ""
+        var age: Int = -1
+        var email: String = ""
+        var sex: Char = 'U'
+        var facebookUsername: String = ""
+
+        constructor(username: String, rank: Int, profilePictureURL: String, club: String, age: Int,
+                    email: String, sex: Char, facebookUsername: String){
+            this.username = username
+            this.rank= rank
+            this.profilePictureURL= profilePictureURL
+            this.club= club
+            this.age= age
+            this.email= email
+            this.sex= sex
+            this.facebookUsername= facebookUsername
+
+        }
+    }
+    class Request {
+        var username: String = ""
+
+        constructor(username: String) {
+            this.username= username
+        }
+
+    }
+    class ViewModel {
+        var message: String= ""
+
+        constructor(message: String){
+            this.message= message
+        }
+    }
+}
