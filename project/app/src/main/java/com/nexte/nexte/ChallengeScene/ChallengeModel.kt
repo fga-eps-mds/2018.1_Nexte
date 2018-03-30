eb7aefb39031c67beed424cbfad8b5e2070faba7
Alexandre Miguel <aleronupe@gmail.com>
2018-03-30 01:54:31 -0300

    Created ChallengeScene package
    * Created interactor, model, presenter, view and worker classes

---
+++ project/app/src/main/java/com/nexte/nexte/ChallengeScene/ChallengeModel.kt
@@ -0,0 +1,24 @@
+package com.nexte.nexte.ChallengeScene
+
+/**
+ * Created by Alexandre Miguel at 30/03/2018
+ */
+
+
+
+class ChallengeModel{
+
+
+    class Request{
+
+        constructor(place: String)
+
+    }
+
+    class Response{}
+
+    class ViewModel{}
+
+
+
+}
