53308e5f1ef129b709077467b06c10290d514afb
Alexandre Miguel <aleronupe@gmail.com>
2018-04-05 00:43:50 -0300

    Recreated ShowProfileActivity
    * Recreated ShowProfileActivity to solve problems on the last version
    * Added picture and TextView attributes on activity_show_profile.xml
    * Added constraints applied to the attributes
    
    Co-authored-by: Letícia Meneses <lmbs.geo@gmail.com>

--- project/app/src/main/java/com/nexte/nexte/ShowProfileScene/ShowProfileActivity.kt
+++ project/app/src/main/java/com/nexte/nexte/ShowProfileScene/ShowProfileActivity.kt
@@ -1,24 +1,15 @@
 package com.nexte.nexte.ShowProfileScene
 
-import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 import com.nexte.nexte.R
-
-
-import kotlinx.android.synthetic.main.activity_show_profile.*
 
 class ShowProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+
     }
-
 }
