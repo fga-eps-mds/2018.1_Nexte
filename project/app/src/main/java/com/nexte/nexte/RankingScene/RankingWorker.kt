a81f997d4de3b0414992c8f6bf2d6050599aca63
helenagoulart <helenabretasg@gmail.com>
2018-04-10 19:41:42 -0300

    Developed Ranking Scene
    * Included new values to Worker class

--- project/app/src/main/java/com/nexte/nexte/RankingScene/RankingWorker.kt
+++ project/app/src/main/java/com/nexte/nexte/RankingScene/RankingWorker.kt
@@ -9,17 +9,39 @@ import com.nexte.nexte.FeedScene.FeedModel
 class RankingWorker {
 
     fun getUsersInRanking(request: RankingModel.Request, completion: (RankingModel.Response) -> Unit) {
-        val response: RankingModel.Response(this.)
-        //The request is empty, so there is no further validation needed.
-
+        val response = RankingModel.Response(this.generateRankingMockData())
 
         completion(response)
 
     }
 
     private fun generateRankingMockData(): Array<RankingModel.RankingActivity> {
-        var players: Array<RankingModel.RankingPlayer> = arrayOf(RankingModel.Player("Gabriel Albino", "someURLHere", 100, 0, "10/10/2010", 1),
-                RankingModel.Player("Helena Goulart", "someURLHere", 99, 1, "10/10/2011", 2))
-        var response: RankingModel.Response = RankingModel.Response(players, request.context)
+
+        val userRanking1 = RankingModel.RankingPlayer("Helena", R.mipmap.ic_launcher, 10, 0, 1)
+        val userRanking2 = RankingModel.RankingPlayer("Leticia", R.mipmap.ic_launcher, 9, 1, 2)
+        val userRanking3 = RankingModel.RankingPlayer("Gabriel", R.mipmap.ic_launcher, 8, 2, 3)
+        val userRanking4 = RankingModel.RankingPlayer("Lorrany", R.mipmap.ic_launcher, 7, 3, 4)
+        val userRanking5 = RankingModel.RankingPlayer("Alexandre", R.mipmap.ic_launcher, 6, 4, 5)
+        val userRanking6 = RankingModel.RankingPlayer("Luis Gustavo", R.mipmap.ic_launcher, 5, 5, 6)
+        val userRanking7 = RankingModel.RankingPlayer("Guilherme", R.mipmap.ic_launcher, 4, 6, 7)
+        val userRanking8 = RankingModel.RankingPlayer("Giovanni", R.mipmap.ic_launcher, 3, 7, 8)
+        val userRanking9 = RankingModel.RankingPlayer("Miguel", R.mipmap.ic_launcher, 2, 6, 9)
+        val userRanking10 = RankingModel.RankingPlayer("Larissa", R.mipmap.ic_launcher, 1, 5, 10)
+
+        val rankingActivity1 = RankingModel.RankingActivity(userRanking1)
+        val rankingActivity2 = RankingModel.RankingActivity(userRanking2)
+        val rankingActivity3 = RankingModel.RankingActivity(userRanking3)
+        val rankingActivity4 = RankingModel.RankingActivity(userRanking4)
+        val rankingActivity5 = RankingModel.RankingActivity(userRanking5)
+        val rankingActivity6 = RankingModel.RankingActivity(userRanking6)
+        val rankingActivity7 = RankingModel.RankingActivity(userRanking7)
+        val rankingActivity8 = RankingModel.RankingActivity(userRanking8)
+        val rankingActivity9 = RankingModel.RankingActivity(userRanking9)
+        val rankingActivity10 = RankingModel.RankingActivity(userRanking10)
+
+        return arrayOf(rankingActivity1, rankingActivity2, rankingActivity3, rankingActivity4,
+                rankingActivity5, rankingActivity6, rankingActivity7, rankingActivity8, rankingActivity9, rankingActivity10)
+
+
     }
 }
