14fcf707327147d82cc54a3f9a22383e992906a4
Lorrany <lorranyfreire@hotmail.com>
2018-04-13 22:56:34 -0300

    Comments added in LikeListInteractor *Comment added in interface LikeListBusinessLogic *Comment added in method fetchDataToList *Comment added LikeListInteractor

--- project/app/src/main/java/com/nexte/nexte/LikeListScene/LikeListInteractor.kt
+++ project/app/src/main/java/com/nexte/nexte/LikeListScene/LikeListInteractor.kt
@@ -10,14 +10,23 @@ import android.app.DownloadManager
  * Interface to define Business Logic to LikeList Class that will used to call this Interactor on other class layer
  */
 interface LikeListBusinessLogic {
+
+
     /**
      * * Method responsable to fetch players informations to list and
      * pass request for the Worker and send response to the Presenter
      *
+     * @param request
      */
     fun fetchDataToList(request: LikeListModel.Request)
 }
 
+/**
+ *  * Class that implements [LikeListBusinessLogic] and is responsible for the communication between worker and presenter
+ *
+ * @property worker Reference to worker [LikeListWorker]
+ * @property presenter Reference to presenter [LikeListPresenter]
+ */
 class LikeListInteractor(var presenter:LikeListPresentationLogic? = null ) : LikeListBusinessLogic {
 
     var worker: LikeListWorker = LikeListWorker()
