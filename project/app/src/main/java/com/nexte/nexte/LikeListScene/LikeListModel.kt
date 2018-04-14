7bba475faeca9bfe449536245fd2acc38bb98ae4
Lorrany <lorranyfreire@hotmail.com>
2018-04-13 21:58:32 -0300

    Comments added *Comments added in LikeListModel *Comments addes in LikeListWorker

--- project/app/src/main/java/com/nexte/nexte/LikeListScene/LikeListModel.kt
+++ project/app/src/main/java/com/nexte/nexte/LikeListScene/LikeListModel.kt
@@ -6,17 +6,30 @@ import java.util.*
 /**
  * Created by lorrany on 12/04/18.
  */
-class LikeListModel {
 
+/**
+ * Class Responsable to add a list of likes
+ */
+class LikeListModel {
     /**
-     * Class Responsable to add a list of likes
+     * Class responsible to pass data of view to interactor and after to worker.
      */
 
     class Request(var request: String)
 
 
+    /**
+     * Class responsible to take the informations of worker to pass for Presenter
+     *
+     * @param PLayers MutableList that hold informations to show in screen(name,photo and time)
+     */
     class Response(var Players: MutableList<Players>)
 
+    /**
+     * Class responsible to define how the list view will display the formatted data, passed to view
+     *
+     * @param PlayersFormatted MutableList that hold informations to show in screen
+     */
 
     class ViewModel(var PlayersFormatted: MutableList<PlayersFormatted> )
 
--- project/app/src/main/java/com/nexte/nexte/LikeListScene/LikeListWorker.kt
+++ project/app/src/main/java/com/nexte/nexte/LikeListScene/LikeListWorker.kt
@@ -6,8 +6,17 @@ import java.util.jar.Attributes
 /**
  * Created by lorrany on 12/04/18.
  */
+
+/**
+ * Class responsible to do request from view, format response and call completion to send data for called class
+ */
 class LikeListWorker {
 
+    /**
+     * Function to fetch feed data of server
+     * @param request
+     * @param completion
+     */
     fun getListLikesPlayers(request: LikeListModel.Request, completion:
     (LikeListModel.Response) -> Unit) {
 
@@ -15,6 +24,11 @@ class LikeListWorker {
         completion(response)
 
     }
+
+    /**
+     * Function to create a fictions users to use in fictional app mode
+     * @return MutableList of addPLayers
+     */
         private fun generateLikeList(): MutableList<LikeListModel.Players> {
 
             val player1 = LikeListModel.Players("Alexandre", R.mipmap.ic_launcher, Date())
