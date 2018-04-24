b4ed13de9a2ebee203a93498361dba67c797a915
Alexandre Miguel <aleronupe@gmail.com>
2018-04-24 15:53:10 -0300

    Refactored CommentsView
    * Created class ViewHolder
    * Created class ClassCommentsAdapter
    
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

--- project/app/src/main/java/com/nexte/nexte/CommentsScene/CommentsView.kt
+++ project/app/src/main/java/com/nexte/nexte/CommentsScene/CommentsView.kt
@@ -1,5 +1,17 @@
 package com.nexte.nexte.CommentsScene
 
+import android.content.Context
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nexte.nexte.R
+import kotlinx.android.synthetic.main.activity_feed_view.*
+
 /**
  * Interface to define Display Logic to CommentsView Class that will
  * receive information from Presenter
@@ -7,4 +19,82 @@ package com.nexte.nexte.CommentsScene
 interface CommentsDisplayLogic {
 
     fun displayComments(viewModel: CommentsModel.ViewModel)
+}
+
+class CommentsView: AppCompatActivity(), CommentsDisplayLogic {
+
+    var interactor: CommentsInteractor? = null
+    var commentsViewAdapter = CommentsAdapter? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed_view)
+
+        this.setUpCommentsScene()
+
+        this.commentsViewAdapter = CommentsView.CommentsAdapter(mutableListOf(), this)
+        commentsRecyclerView.adapter = this.commentsViewAdapter
+        commentsRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    fun setUpCommentsScene {
+
+        val view = this
+        val interactor = CommentsInteractor()
+        val presenter = CommentsPresenter()
+
+        view.interactor = interactor
+        interactor.presenter = presenter
+        presenter.viewController = view
+    }
+
+    override fun displayComments(viewModel: CommentsModel.ViewModel) {
+
+    }
+
+    class CommentsAdapter(comments: MutableList<CommentsModel.CommentFormatted>,
+                          context: Context) : RecyclerView.Adapter<CommentsAdapter.ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+                CommentsView.CommentsAdapter.ViewHolder {
+
+            val view = LayoutInflater.from(context).inflate(R.layout.row_feed, parent,false)
+            return CommentsView.CommentsAdapter.ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: CommentsView.CommentsAdapter.ViewHolder,
+                                      position: Int) {
+
+            holder.bindView(comments[position], context)
+        }
+
+        override fun getItemCount(): Int {
+
+            return this.comments.size
+        }
+
+
+
+
+
+
+
+
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            fun bindView(commentsFormatted: CommentsModel.CommentFormatted) {
+
+
+
+            }
+
+        }
+
+    }
+
+
+
+
 }
