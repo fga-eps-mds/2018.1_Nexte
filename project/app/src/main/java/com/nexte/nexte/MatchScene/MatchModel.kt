a8ae23cd69530788a00a5f8a99ce1f2009c21a6f
mbslet <lmbs.geo@gmail.com>
2018-04-30 17:34:48 -0300

    Changed class MatchData and crated MatchWorker
    * Changed type players
    
    Co-authored-by: Alexandre Miguel <aleronupe@gmail.com>

--- project/app/src/main/java/com/nexte/nexte/MatchScene/MatchModel.kt
+++ project/app/src/main/java/com/nexte/nexte/MatchScene/MatchModel.kt
@@ -1,5 +1,6 @@
 package com.nexte.nexte.MatchScene
 
+import com.nexte.nexte.Player
 import java.util.*
 
 /**
@@ -10,12 +11,13 @@ class MatchModel {
 
     class Request (var matchID: String)
 
-    class Response (var infoMatch: MatchData)
+    class Response (var match: MatchData)
 
     class ViewModel (var matchFormatted: FormattedMatchData)
 
-    class MatchData (var player1: String, var player2: String, var games: String,
-                     var sets: Int, var day: Date, var hour: Date)
+    class MatchData (var player1: Player, var player2: Player, var betterThan: Enum<1, 3, 5, "WO">,
+                     var sets: Int, var matchDate: Date)
+                             
 
     class FormattedMatchData (var player1: String, var player2: String, var games: String,
                               var sets: String, var day: String, var hour: String)
--- project/app/src/main/java/com/nexte/nexte/MatchScene/MatchWorker.kt
+++ project/app/src/main/java/com/nexte/nexte/MatchScene/MatchWorker.kt
@@ -12,7 +12,7 @@ class MatchWorker {
         completion(response)
     }
 
-    private fun generateMatchData() {
-        
+     fun generateMatchData() {
+
     }
 }
