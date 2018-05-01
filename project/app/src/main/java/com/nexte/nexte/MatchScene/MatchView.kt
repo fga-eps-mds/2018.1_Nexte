5c9e82bf209d0e660be995a42ea10440f898cd5c
mbslet <lmbs.geo@gmail.com>
2018-05-01 15:13:09 -0300

    Implemented MatchView
    Co-authored-by: Alexandre Miguel <aleronupe@gmail.com>

--- project/app/src/main/java/com/nexte/nexte/MatchScene/MatchView.kt
+++ project/app/src/main/java/com/nexte/nexte/MatchScene/MatchView.kt
@@ -3,7 +3,13 @@ package com.nexte.nexte.MatchScene
 import android.support.v7.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 import com.nexte.nexte.R
+import android.view.View
+import android.view.ViewGroup
+import android.content.Context
+import android.view.LayoutInflater
+import java.util.zip.Inflater
 
 
 interface MatchDisplayLogic {
@@ -15,15 +21,19 @@ interface MatchDisplayLogic {
 class MatchView : AppCompatActivity(), MatchDisplayLogic {
 
     var interactor: MatchInteractor? = null
+    var matchViewAdapter: ViewAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_match_view)
 
+        this.setUpMatchScene()
+
 
 
     }
+
 
     private fun setUpMatchScene() {
 
@@ -36,6 +46,26 @@ class MatchView : AppCompatActivity(), MatchDisplayLogic {
         presenter.viewController = view
 
 
+    }
+
+    class MatchDataAdapter (private val matchInfo: MatchModel.FormattedMatchData,
+                            private val context: Context): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        override fun getItemViewType(position: Int): Int {
+            if (position == 0) {
+                return R.layout.row_match_info
+            }
+            else if (position == 1) {
+                return R.layout.row_match_sets
+            }
+            else
+                return R.layout.row_match_time
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+
+            val holder = RecyclerView.ViewHolder
+         }
     }
 
 
