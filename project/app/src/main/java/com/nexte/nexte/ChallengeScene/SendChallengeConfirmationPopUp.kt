ec57c6b2e682d23e252af69a9583a8efefb0e25c
Larissa Sales <larissa.s.sales@outlook.com>
2018-05-03 01:19:09 -0300

    Implemented pop up view
    * Implemented displayMetrics method
    * Created new value style

--- project/app/src/main/java/com/nexte/nexte/ChallengeScene/SendChallengeConfirmationPopUp.kt
+++ project/app/src/main/java/com/nexte/nexte/ChallengeScene/SendChallengeConfirmationPopUp.kt
@@ -2,6 +2,7 @@ package com.nexte.nexte.ChallengeScene
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
 import com.nexte.nexte.R
 
 class SendChallengeConfirmationPopUp : AppCompatActivity() {
@@ -10,5 +11,13 @@ class SendChallengeConfirmationPopUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.send_challenge_confirmation_pop_up)
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        val width = displayMetrics.widthPixels
+        val height = displayMetrics.heightPixels
+
+        window.setLayout((width*.8).toInt(), (height*.6).toInt())
     }
 }
