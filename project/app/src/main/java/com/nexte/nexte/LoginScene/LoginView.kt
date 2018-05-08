46d1829a117b1d65e1233e10f199e56db59ff52e
Miguel Pimentel <miguelpimentel2013@gmail.com>
2018-05-08 17:41:17 -0300

    Solved bugs with account kit

--- project/app/src/main/java/com/nexte/nexte/LoginScene/LoginView.kt
+++ project/app/src/main/java/com/nexte/nexte/LoginScene/LoginView.kt
@@ -3,13 +3,14 @@ package com.nexte.nexte.LoginScene
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import com.facebook.accountkit.AccountKit
 import com.facebook.accountkit.AccountKitLoginResult
 import com.facebook.accountkit.ui.AccountKitConfiguration
 import com.facebook.accountkit.ui.AccountKitActivity
 import com.facebook.accountkit.ui.LoginType
 import com.nexte.nexte.R
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login_view.*
 
 interface LoginDisplayLogic {
@@ -28,75 +29,30 @@ class LoginView : AppCompatActivity(), LoginDisplayLogic {
         setContentView(R.layout.activity_login_view)
         this.setup()
 
-        AccountKit.initialize(this) // Account Kit
-
-        requestAuthentication.setOnClickListener {
-
-            this.createAuthenticationRequest()
+        btnLoginPhonenumber.setOnClickListener {
+            this.loginPhoneNumber()
         }
 
-    }
-
-    /**
-     * Method responsible for creating the authetication request an passing it to the interactor
-     */
-    fun createAuthenticationRequest(){
-        val account = accountField.text.toString()
-        val password = passwordField.text.toString()
-
-        val request: LoginModel.Request = LoginModel.Request(account, password)
-        this.interactor?.doAuthentication(request)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-
-        if (requestCode == LoginModel.AuthenticationStatus.FACEBOOK_REQUEST.value) {
-
-            val loginResult = data!!.getParcelableExtra<AccountKitLoginResult>(AccountKitLoginResult.RESULT_KEY)
-            var message = ""
-
-            if (loginResult.error != null) {
-            } else if (loginResult.wasCancelled()) {
-                message = "Login cancelled"
-            } else {
-                if (loginResult.accessToken != null) {
-                    message = "Sucess" + loginResult.accessToken
-                } else {
-                    println(loginResult.authorizationCode)
-                }
-            }
+        btnLoginFacebook.setOnClickListener {
+            this.loginByEmail()
         }
     }
 
     override fun displayAuthenticateState(viewModel: LoginModel.ViewModel) { }
 
-    fun phoneLogin(view: View) {
+    /**
+     * Method responsible for creating the authetication request an passing it to the interactor
+     */
+//    fun createAuthenticationRequest(){
+//        val account = accountField.text.toString()
+//        val password = passwordField.text.toString()
+//
+//        val request: LoginModel.Request = LoginModel.Request(account, password)
+//        this.interactor?.doAuthentication(request)
+//    }
 
-        val intent = Intent(this, AccountKitActivity::class.java)
-        val configurationBuilder = AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE,
-                                                                                          AccountKitActivity.ResponseType.CODE)
-        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
-                        configurationBuilder.build())
-
-        this.startActivityForResult(intent, LoginModel.AuthenticationStatus.FACEBOOK_REQUEST.value)
-    }
-
-    fun emailLogin(view: View) {
-
-        val intent = Intent(this, AccountKitActivity::class.java)
-
-        val configurationBuilder = AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.EMAIL,
-                AccountKitActivity.ResponseType.CODE)
-        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
-                configurationBuilder.build())
-
-        this.startActivityForResult(intent, LoginModel.AuthenticationStatus.FACEBOOK_REQUEST.value)
-    }
 
     fun setup() {
-
         val view = this
         val interactor = LoginInteractor()
         val presenter = LoginPresenter()
@@ -104,5 +60,87 @@ class LoginView : AppCompatActivity(), LoginDisplayLogic {
         view.interactor = interactor
         interactor.presenter = presenter
         presenter.view = view
+    }
+
+    // Account Kit
+
+    companion object {
+        val RC_PHONE_NUMBER = 1
+        val RC_FACEBOOK_ACCOUNT = 2
+    }
+
+    fun loginPhoneNumber() {
+        val intent = Intent(this, AccountKitActivity::class.java)
+        val configBuilder = AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE,
+                AccountKitActivity.ResponseType.TOKEN)
+
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, configBuilder.build())
+        startActivityForResult(intent, RC_PHONE_NUMBER)
+    }
+
+    fun loginByEmail() {
+        val intent  = Intent(this, AccountKitActivity::class.java)
+        val builder = AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.EMAIL,
+                AccountKitActivity.ResponseType.TOKEN)
+
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION, builder.build())
+        startActivityForResult(intent, RC_PHONE_NUMBER)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            RC_PHONE_NUMBER -> {
+                Log.d("debug", "user request phone number")
+
+                val loginResult = data?.getParcelableExtra<AccountKitLoginResult>(AccountKitLoginResult.RESULT_KEY)
+                var toastMsg: String?
+
+                if (loginResult != null) {
+                    Log.d("debug", "login result ${loginResult.accessToken}")
+                    toastMsg = handleLoginResult(loginResult)
+                } else {
+                    toastMsg = "login gagal"
+                }
+
+                showMessage(toastMsg)
+
+            }
+            RC_FACEBOOK_ACCOUNT -> {  }
+        }
+    }
+
+    private fun handleLoginResult(loginResult: AccountKitLoginResult): String? {
+        var msg: String? = null
+
+        if (loginResult.error != null) {
+            Log.e("debug", "login error")
+            msg = loginResult?.error?.errorType?.message
+        } else if (loginResult.wasCancelled()) {
+            Log.d("debug", "login cancelled")
+            msg = "login cancel"
+        } else {
+            if (loginResult.accessToken != null) {
+                val accessToken = loginResult.accessToken
+                val accountId = accessToken?.accountId
+                if (accountId != null) {
+                    msg = "success $accountId"
+                    Log.d("debug", "acces token ${accessToken.token}")
+                } else {
+                    Log.e("debug", "account id null")
+                }
+            } else {
+                Log.e("debug", "access token null")
+            }
+        }
+
+        return msg
+    }
+
+    private fun showMessage(msg: String?) {
+        if (msg != null) {
+            Log.d("debug", "message $msg")
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
     }
 }
