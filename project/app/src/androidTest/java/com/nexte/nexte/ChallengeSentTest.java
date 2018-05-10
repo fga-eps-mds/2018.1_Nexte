f0191b2d899b241b27d38799165ba026997848d0
Alexandre Miguel <aleronupe@gmail.com>
2018-05-10 12:31:00 -0300

    Recreated class to test ChallengeSentTest

--- project/app/src/androidTest/java/com/nexte/nexte/ChallengeSentTest.java
+++ project/app/src/androidTest/java/com/nexte/nexte/ChallengeSentTest.java
@@ -1,14 +1,11 @@
 package com.nexte.nexte;
 
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.runner.RunWith;
 
-@LargeTest
 @RunWith(AndroidJUnit4.class)
-public class ChallengeSentNoPlayersTest {
+public class ChallengeSentTest {
 
-
-
+    private
 }
