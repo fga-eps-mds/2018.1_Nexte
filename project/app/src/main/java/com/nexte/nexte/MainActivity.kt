fbafc44f85c4012d254bb49256a549236e7beb9e
helenagoulart <helenabretasg@gmail.com>
2018-05-11 03:13:12 -0300

    Started to include the activities on bottom navigation * Called id icons on Main Activity
    Co-authored-by: Leticia Meneses <lmbs.geo@gmail.com>

--- project/app/src/main/java/com/nexte/nexte/MainActivity.kt
+++ project/app/src/main/java/com/nexte/nexte/MainActivity.kt
@@ -3,11 +3,13 @@ package com.nexte.nexte
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
@@ -32,6 +34,24 @@ class MainActivity : AppCompatActivity(), LoginDisplayLogic {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_nav_view)
+        BottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener);
+        @Override
+        public boolean OnNavigationItemSelectedListener(@NonNull MenuItem item) {
+            switch(item.getItemId()) {
+                case R.id.feed:
+                // include activity
+                break
+                case R.id.challenge:
+                // include activity
+                break
+                case R.id.ranking:
+                // include activity
+                break
+            }
+            return true
+        }
 
         // Setting up feed button listener
         /*feedButton.setOnClickListener {
@@ -77,99 +97,6 @@ class MainActivity : AppCompatActivity(), LoginDisplayLogic {
     // Print a message according with received data
     override fun displayAuthenticateState(viewModel: LoginModel.ViewModel) {}
 
-
-    // Helena: comecei a fazer aqui!
-    class TabFragment : Fragment() {
-
-        var position = 0
-        private var feedRecyclerView: RecyclerView? = null
-
-        /**
-         * Method that gets which tab is selected by the user
-         */
-        fun getInstance(position: Int): ChallengeView.TabFragment {
-
-            val bundle = Bundle()
-            val tabFragment = ChallengeView.TabFragment()
-
-            bundle.putInt("position", position)
-            tabFragment.arguments = bundle
-            return tabFragment
-        }
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-
-            super.onCreate(savedInstanceState)
-            position = arguments.getInt("position")
-        }
-
-        override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
-            val view: View?
-
-            if(position == 0) {
-                view = inflater?.inflate(R.layout.activity_feed_view, container, false)
-                 val feedViewAdapter = FeedView.FeedAdapter(mutableListOf(), context)
-                feedRecyclerView= view?.findViewById(R.id.feedRecyclerView)
-                feedRecyclerView?.adapter = feedViewAdapter
-                feedRecyclerView?.layoutManager = LinearLayoutManager(context)
-
-                .createGetActivitiesRequest()
-
-            } else if(position == 1){
-                view = inflater?.inflate(R.layout.activity_challenger, container, false)
-                recyclerView = view?.findViewById(R.id.challengeRecyclerView)
-                this.sendChallengeButton = view?.findViewById(R.id.sendChallengeButton)
-                (context as ChallengeView).sendChallengeButton = this.sendChallengeButton
-                (context as ChallengeView).expandedLosses = view?.findViewById(R.id.expandedLosses)
-                (context as ChallengeView).expandedName = view?.findViewById(R.id.expandedName)
-                (context as ChallengeView).expandedRankingTextView = view?.findViewById(R.id.expandedRankingTextView)
-                (context as ChallengeView).expandedWins = view?.findViewById(R.id.expandedWins)
-            } else {
-                view = inflater?.inflate(R.layout.activity_ranking, container,  false)
-            }
-            return view
-        }
-
-        // Acho que aqui podemos setar o clique do bottom navigation, mas não to sabendo
-        // chamar os parametros do que tá no xml dele
-
-        override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-
-            super.onViewCreated(view, savedInstanceState)
-
-            feedActivity?.setOnClickListener {
-                val  =
-//                        ChallengeModel.ChallengeButtonRequest.Request(this.expandedName.text.toString())
-//                (context as ChallengeView).interactor?.requestMessageForChallenger(request)
-            }
-
-            val request = ChallengeModel.ShowRankingPlayersRequest.Request(UserSingleton.getUserInformations().rankingPosition)
-            (context as ChallengeView).interactor?.requestPlayersToChallenge(request)
-        }
-//        class ViewPagerAdapter (fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
-//
-//            private val pagePosition =
-//
-//            override fun getCount(): Int {
-//
-//                return pagePosition.size
-//            }
-//
-//            override fun getItem(position: Int): Fragment {
-//
-//                val tabFragment = TabFragment()
-//
-//                return tabFragment.getInstance(position)
-//            }
-//
-//            override fun getPageTitle(position: Int): CharSequence {
-//
-//                return pagePosition.elementAt(position)
-//            }
-//        }
-
-
     }
 
-}
+
