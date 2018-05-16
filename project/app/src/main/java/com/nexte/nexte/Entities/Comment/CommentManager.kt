6fc05f1ba6729d7e8be25c0fd900098cdcdec595
helenagoulart <helenabretasg@gmail.com>
2018-05-16 00:47:36 -0300

    Included new functions on CommentManager
    Co-authored-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

--- project/app/src/main/java/com/nexte/nexte/Entities/Comment/CommentManager.kt
+++ project/app/src/main/java/com/nexte/nexte/Entities/Comment/CommentManager.kt
@@ -1,7 +1,8 @@
 package com.nexte.nexte.Entities.Comment
-import com.nexte.nexte.Entities.Challenge.Challenge
 
-class CommentManager {
+import com.nexte.nexte.Mocker.CommentsMocker
+
+class CommentManager(val commentAdapter: CommentAdapter = CommentAdapterRealm()) {
     fun get(identifier: String): Comment? {
         return commentAdapter.get(identifier)
     }
@@ -13,6 +14,37 @@ class CommentManager {
     fun update(comment: Comment): Comment? {
         return commentAdapter.updateOrInsert(comment)
     }
+
+    fun updateMany(comments: List<Comment>): List<Comment> {
+        val newComments: MutableList<Comment> = mutableListOf()
+        for (comment in comments) {
+            val updatedComment = commentAdapter.updateOrInsert(comment)
+            updatedComment?.let {
+                newComments.add(it)
+            }
+        }
+        return newComments.toList()
+    }
+
+    fun delete(identifier: String): Comment? {
+        return commentAdapter.delete(identifier)
+    }
+
+    fun createInitialMocker(): List<Comment> {
+        val commentsInMocker = CommentsMocker.generateCommentsList() // nao achei a mocker dele
+        val insertedComment: MutableList<Comment> = mutableListOf()
+
+        for (comment in commentsInMocker) {
+
+            val insertedComments = commentAdapter.updateOrInsert(comment)
+            insertedComments?.let {
+                insertedComments.add(it)
+            }
+        }
+
+        return insertedComment.toList()
+    }
+
 
 
 }
