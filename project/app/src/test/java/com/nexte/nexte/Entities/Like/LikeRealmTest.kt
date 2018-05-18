f5dcb8c7704676b20ad5ef625f0d134f262c469a
helenagoulart <helenabretasg@gmail.com>
2018-05-18 11:18:18 -0300

    Included tests in LikeRealm * Created tests for all of her functions
    Co-authored-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

---
+++ project/app/src/test/java/com/nexte/nexte/Entities/Like/LikeRealmTest.kt
@@ -0,0 +1,62 @@
+package com.nexte.nexte.Entities.Like
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import java.util.*
+
+
+class StoryRealmTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun testInstatiateStoryRealm(){
+        //prepare
+        val id = "1"
+        val userId = "1"
+        val date = Date()
+        //call
+        val likeRealm = LikeRealm(id, userId, date)
+
+        //assert
+        Assert.assertEquals(id, likeRealm.id)
+        Assert.assertEquals(userId, likeRealm.userId)
+        Assert.assertEquals(date, likeRealm.date)
+    }
+
+    @Test
+    fun successRequestsEnumTest() {
+
+        // Prepare
+        val likeRealmRequest = LikeRealm.ServerRequest.LIKE.request
+
+        // Asserts
+        Assert.assertEquals("Like request is incorrect!", likeRealmRequest.keys, setOf("route", "method"))
+        }
+
+    @Test
+    fun testStoryRealmSet(){
+        //prepare
+        val id = "1"
+        val userId = "1"
+        val date = Date()
+
+        //call
+        val likeRealm = LikeRealm()
+        likeRealm.id = id
+        likeRealm.userId = userId
+        likeRealm.date = date
+
+        //assert
+        Assert.assertEquals(id, likeRealm.id)
+        Assert.assertEquals(userId, likeRealm.userId)
+        Assert.assertEquals(date, likeRealm.date)
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
