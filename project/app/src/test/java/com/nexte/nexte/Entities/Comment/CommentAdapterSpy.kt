6694c7811cc077638a2ae018c36a4ccd63ecb3bb
helenagoulart <helenabretasg@gmail.com>
2018-05-18 23:58:53 -0300

    Included tests for CommentRealm * Created a CommentSpy testing all of her functions
    Co-authored-by: Luis Gustavo <luis.gustavo3013@hotmail.com>

---
+++ project/app/src/test/java/com/nexte/nexte/Entities/Comment/CommentAdapterSpy.kt
@@ -0,0 +1,45 @@
+package com.nexte.nexte.Entities.Comment
+import java.util.*
+
+class CommentAdapterSpy: CommentAdapter {
+
+    override fun delete(identifier: String): Comment? {
+        if (identifier == "1") {
+            return mockComment()
+        } else {
+            return null
+        }
+    }
+
+    override fun get(identifier: String): Comment? {
+        if (identifier == "1") {
+            return mockComment()
+        } else {
+            return null
+        }
+    }
+
+    override fun getAll(): List<Comment> {
+        val commentList: MutableList<Comment> = mutableListOf(mockComment(), mockComment(), mockComment(), mockComment())
+        return commentList.toList()
+    }
+
+    override fun updateOrInsert(challenge: Comment): Comment? {
+        if (challenge.id == "1") {
+            return mockComment()
+        } else {
+            return null
+        }
+    }
+
+    private fun mockComment(): Comment {
+        val id: String? = "1"
+        val userId: String? = "2"
+        val comment: String? = ""
+        val date: Date? = Date()
+
+        val comments = Comment(id, userId, comment, date)
+
+        return comments
+    }
+}
