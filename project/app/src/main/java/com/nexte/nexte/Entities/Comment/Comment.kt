fbf90fe6ae3c4c84f805e7031cd619ee03f07d6e
Luis Gustavo <luis.gustavo3013@hotmail.com>
2018-06-05 08:53:20 -0300

    Created method that tranforns a json into a comment

--- project/app/src/main/java/com/nexte/nexte/Entities/Comment/Comment.kt
+++ project/app/src/main/java/com/nexte/nexte/Entities/Comment/Comment.kt
@@ -1,4 +1,7 @@
 package com.nexte.nexte.Entities.Comment
+import android.annotation.SuppressLint
+import org.json.JSONObject
+import java.text.SimpleDateFormat
 import java.util.*
 
 /**
@@ -19,5 +22,26 @@ open class Comment(var id: String? = null,
         ADD_COMMENT(hashMapOf("route" to "comment", "method" to "post")),
         DELETE_COMMENT(hashMapOf("route" to "comment", "method" to "delete")),
         REPORT_COMMENT(hashMapOf("route" to "report-comment", "method" to "post"))
+    }
+
+    companion object {
+
+        @SuppressLint("SimpleDateFormat")
+                /**
+                 * Method used to tranform a jsonObject(received from api) to a Comment
+                 *
+                 * @param jsonComment jsonObject that has user data
+                 *
+                 * @return comment
+                 */
+        fun createCommentFromJsonObject(jsonComment: JSONObject): Comment {
+            val id = jsonComment["id"] as String
+            val userId = jsonComment["userID"] as String
+            val date = SimpleDateFormat("dd-MM-yyyy")
+                    .parse(jsonComment["date"] as String)
+            val comment = jsonComment["comment"] as String
+
+            return Comment(id, userId, comment, date)
+        }
     }
 }
