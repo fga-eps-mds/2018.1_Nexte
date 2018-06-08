a03c5ea9bd23f33ec3171a3eeef73e4e9573216d
Alexandre Miguel <aleronupe@gmail.com>
2018-06-08 18:42:04 -0300

    Created instrumentation tests for successful cases
    Co-authored-by: Lorrany Freire <lorranyfreire@hotmail.com>

---
+++ project/app/src/androidTest/java/com/nexte/nexte/MatchScene/TestWOChallengerSuccess.java
@@ -0,0 +1,138 @@
+package com.nexte.nexte.MatchScene;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.nexte.nexte.MainActivity;
+import com.nexte.nexte.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class TestWOChallengerSuccess {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testWOChallengerSuccess() {
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(ViewMatchers.withId(R.id.challenge),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.bottom_nav_view),
+                                        0),
+                                1),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        ViewInteraction appCompatImageView = onView(
+                allOf(withId(R.id.userPicture), withContentDescription("User picture"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.challengeRecyclerView),
+                                        1),
+                                0),
+                        isDisplayed()));
+        appCompatImageView.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.sendChallengeButton), withText("Enviar Desafio"),
+                        childAtPosition(
+                                withParent(withId(R.id.challengerviewpager)),
+                                5),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(android.R.id.button1), withText("Ok"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        appCompatButton2.perform(scrollTo(), click());
+
+        ViewInteraction radioRealButton = onView(
+                allOf(withId(R.id.buttonWO), withText("WO"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.buttonGroup),
+                                        2),
+                                3),
+                        isDisplayed()));
+        radioRealButton.perform(click());
+
+        ViewInteraction radioRealButton2 = onView(
+                allOf(withId(R.id.buttonChallenger), withText("Desafiante"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.buttonGroupWO),
+                                        2),
+                                0),
+                        isDisplayed()));
+        radioRealButton2.perform(click());
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.sendButton), withText("Send  Challenge"),
+                        childAtPosition(
+                                withParent(withId(R.id.challengerviewpager)),
+                                1),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(android.R.id.button1), withText("Ok"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                3)));
+        appCompatButton4.perform(scrollTo(), click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
