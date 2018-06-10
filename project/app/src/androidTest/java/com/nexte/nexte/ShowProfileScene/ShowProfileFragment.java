224739258137f9fa558867ed2fc4c769ca3ee88a
Gabriel Albino <enggabrielalbino@gmail.com>
2018-06-10 00:16:49 -0300

    Added instrumentation test
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

--- project/app/src/androidTest/java/com/nexte/nexte/ShowProfileScene/ShowProfileFragment.java
+++ project/app/src/androidTest/java/com/nexte/nexte/ShowProfileScene/ShowProfileFragment.java
@@ -1,4 +1,5 @@
-package com.nexte.nexte.PlayersListScene;
+package com.nexte.nexte.ShowProfileScene;
+
 
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.matcher.ViewMatchers;
@@ -15,37 +16,35 @@ import com.nexte.nexte.R;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
-import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class SendChallengeInstrumentationTest {
+public class ShowProfileFragment {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void sendChallengeInstrumentationTest() {
+    public void showProfileFragment() {
         ViewInteraction appCompatButton = onView(
-                allOf(ViewMatchers.withId(R.id.challangeButton), withText("Desafios"),
+                allOf(ViewMatchers.withId(R.id.navigationLogin), withText("Navegar pelo NEXTE!"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
-                                5),
+                                3),
                         isDisplayed()));
         appCompatButton.perform(click());
 
@@ -53,41 +52,35 @@ public class SendChallengeInstrumentationTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(700);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        ViewInteraction appCompatImageView = onView(
-                allOf(withId(R.id.userPicture), withContentDescription("User picture"),
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.profileNavigation),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(R.id.challengeRecyclerView),
-                                        1),
+                                        withId(R.id.bottom_navigation),
+                                        0),
+                                3),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        onView(withId(R.id.lineChart)).check(matches(isDisplayed()));
+
+        ViewInteraction imageView = onView(
+                allOf(withId(R.id.imageView),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
                                 0),
                         isDisplayed()));
-        appCompatImageView.perform(click());
-
-        ViewInteraction appCompatButton2 = onView(
-                allOf(withId(R.id.sendChallengeButton), withText("Enviar Desafio"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                5),
-                        isDisplayed()));
-        appCompatButton2.perform(click());
-
-        ViewInteraction appCompatButton3 = onView(
-                allOf(withId(android.R.id.button1), withText("Ok"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withClassName(is("android.widget.ScrollView")),
-                                        0),
-                                3)));
-        appCompatButton3.perform(scrollTo(), click());
+        imageView.check(matches(isDisplayed()));
 
     }
+
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
