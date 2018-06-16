7aa23b169a5f83c0481e383398445a58ad4d7678
helenagoulart <helenabretasg@gmail.com>
2018-06-16 01:00:36 -0300

    Included design of first scene from Onboarding for users without login on app
    Co-authored-by: Gabriel Albino <enggabrielalbino@gmail.com>

---
+++ project/app/src/main/java/com/nexte/nexte/UserOnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.nexte.nexte
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserOnBoardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_on_boarding)
+    }
+}
